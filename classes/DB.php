17bc7f4b314b0daa2a01927dd74c5c21879ba9ff
sydorenkovd <sydorenkovd@gmail.com>
2015-12-09 22:45:20 +0200

    DB(Singelton)

--- classes/DB.php
+++ classes/DB.php
@@ -1,7 +1,22 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Виктор Сидоренко
- * Date: 09.12.2015
- * Time: 21:41
- */
+class DB{
+    private static $_instance = null;
+    private $_pdo,
+        $_query,
+        $_error = false,
+        $_results,
+        $_count = 0;
+    private function __construct(){
+            try{
+                $this->_pdo = new PDO('mysql:host='. Config::get('mysql/host') . ';dbname=' . Config::get('mysql/db'), Config::get('mysql/username'), Config::get('mysql/password'));
+            } catch(PDOException $e){
+            die($e->getMessage());
+        }
+    }
+    public static function connect(){
+        if(!isset(self::$_instance)){
+            self::$_instance = new self();
+        }
+        return self::$_instance;
+    }
+}
