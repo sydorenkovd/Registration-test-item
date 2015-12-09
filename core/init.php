b7d3139264a81cabc12e2fbe2931faa5234ce488
sydorenkovd <sydorenkovd@gmail.com>
2015-12-09 22:02:39 +0200

    config and autoload

--- core/init.php
+++ core/init.php
@@ -1,7 +1,23 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Виктор Сидоренко
- * Date: 09.12.2015
- * Time: 21:45
- */
+session_start();
+
+$GLOBALS['config'] = [
+  'mysql' => [
+      'host' =>'127.0.0.1',
+      'username' => 'root',
+      'password' => '',
+      'db' => 'registration'
+  ],
+    'remember' => [
+        'cookie_name' => 'hash',
+        'cookie_expiry' => 604800,
+    ],
+    'session' => [
+        'session_name' => 'user'
+    ]
+];
+
+spl_autoload_register(function($class){
+    require_once 'classes/' . $class . '.php';
+});
+require_once "functions/sanitize.php";
--- index.php
+++ index.php
@@ -1,7 +1,2 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Виктор Сидоренко
- * Date: 09.12.2015
- * Time: 21:38
- */
+require_once 'core/init.php';
